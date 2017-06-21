6330176f9ee3ec8eb14ed505a00de7769303f337
arsalan17chaudhry <arsalan17@live.com>
2017-06-21 16:07:58 -0500

    Implemented code for photo upload on my profile

--- SeleniumLocalTests/QA-DMC/qatesting/qaTestByArsalan/src/qaTestByArsalan/testBatch2.java
+++ SeleniumLocalTests/QA-DMC/qatesting/qaTestByArsalan/src/qaTestByArsalan/testBatch2.java
@@ -1,8 +1,15 @@
 package qaTestByArsalan;
 
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Field;
 import java.util.concurrent.TimeUnit;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,7 +20,7 @@ public class testBatch2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Create any variables
+		//Create any Variables
 		String Selenium_User1 = System.getenv("Selenium_User1");
 		String Selenium_Pass1 = System.getenv("Selenium_Pass1");
 
@@ -30,7 +37,7 @@ public class testBatch2 {
 			WebDriver driver= new ChromeDriver();
 
 			//Wait created
-			 WebDriverWait wait = new WebDriverWait(driver, 10);
+			 WebDriverWait wait = new WebDriverWait(driver, 30);
 
 			//Maximize browser
 			driver.manage().window().maximize();
@@ -79,13 +86,17 @@ public class testBatch2 {
 	}//end of login()
 
 	public static void myProfile(WebDriver driver, WebDriverWait wait){
+			//Declare any Variables
+		 	String documentLocation = "/home/arsalan/Pictures/office.jpg";
+		 
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //wait
 
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //wait
-
-		//driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div/div[3]/md-menu/button/span")).click();
-		//driver.findElement(By.xpath("/html/body/div[6]/md-menu-content/md-menu-item[2]/a/span")).click();
-	         driver.navigate().refresh();
-			 driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
+			//driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div/div[3]/md-menu/button/span")).click();
+			//driver.findElement(By.xpath("/html/body/div[6]/md-menu-content/md-menu-item[2]/a/span")).click();
+	        driver.navigate().refresh();
+			 
+	        sleepThreadWait();
+	         
 			 driver.navigate().to("https://portal.opendmc.org/profile.php#/538");
 			 
 			 //Click edit profile 
@@ -101,7 +112,7 @@ public class testBatch2 {
 			 //Job Title
 			 driver.findElement(By.xpath("//*[@id='input_44']")).clear();
 			 driver.findElement(By.xpath("//*[@id='input_44']")).sendKeys("DMCIntern");
-			 */ 
+			 
 			//Location 
 			 driver.findElement(By.xpath("//*[@id='input_46']")).clear();
 			 driver.findElement(By.xpath("//*[@id='input_46']")).sendKeys("UI LABS TEST");
@@ -113,18 +124,81 @@ public class testBatch2 {
 			 //My Skills
 			 driver.findElement(By.xpath("/html/body/div[2]/div/md-content/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[2]/span")).click();
 			 driver.findElement(By.xpath("//*[@id='input_50']")).sendKeys("Test"); //enter skill
-			 driver.findElement(By.xpath("//*[@id='tab-content-99']/div/md-content/div/form/button/span")).click();//add
-			 
+			 driver.findElement(By.xpath("//*[@id='tab-content-48']/div/md-content/div/form/button/span")).click();//add
+			 */
 			 //Upload and Change Photo
-			 driver.findElement(By.xpath("/html/body/div[2]/div/md-content/div/div[1]/button")).click(); //hit change photo
-			 driver.findElement(By.xpath("//*[@id='documentDropZone']/div/div/ng-md-icon/svg")).sendKeys("UI_LABS_3.jpg");//document drop 
+			 //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //wait
+			 sleepThreadWait();
 			 
-			 //UI_LABS_3.jpg
-			 //driver.findElement(By.id("profile_pic_10")).sendKeys("C:\\Users\\Anupam\\Desktop\\aaa.png");
-			 //driver.findElement(By.xpath("/html/body/div[2]/div/md-content/div/div[2]/div[2]/button[2]/span")).click();
-			
+			 driver.findElement(By.xpath("/html/body/div[2]/div/md-content/div/div[1]/button")).click(); //hit change photo
+			 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //wait
+			 driver.findElement(By.xpath("//*[@id='documentDropZone']/div/div/span[2]")).click();//document drop 
+			 
+			 fileLocationUpload(documentLocation);
 	}//end of myProfile
 
+	
+	public static void fileLocationUpload(String documentLocation){
+		//Go to search bar
+		try{
+			Robot robot1 = new Robot();
+			robot1.delay(1000);
+			
+			for(int i = 0; i < 3; i++){
+				robot1.keyPress(KeyEvent.VK_TAB);
+				robot1.keyRelease(KeyEvent.VK_TAB);	
+				robot1.delay(1000);
+			}
+
+			robot1.delay(1000);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	
+		//Go to location of file
+			try{
+				StringSelection stringSelection = new StringSelection(documentLocation);
+				Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+				clipboard.setContents(stringSelection, stringSelection);
+
+				Robot robot2 = new Robot();
+				robot2.keyPress(KeyEvent.VK_CONTROL);
+				robot2.keyPress(KeyEvent.VK_V);
+				robot2.keyRelease(KeyEvent.VK_V);
+				robot2.keyRelease(KeyEvent.VK_CONTROL);
+
+				
+			}catch(Exception e){
+				System.out.println(e);
+			}
+			
+		//Enter
+		try{
+			Robot robot3 = new Robot();
+			robot3.keyPress(KeyEvent.VK_ENTER);
+			robot3.keyRelease(KeyEvent.VK_ENTER);
+			robot3.delay(1000);
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+																																																	
+	}//end of fileLocationUpload()
+	
+	
+	//Wait for the mentioned seconds
+	public static void sleepThreadWait(){
+		try {
+			Thread.sleep(6000);
+			
+		}catch(InterruptedException e){
+			
+			e.printStackTrace();
+		}
+	}//end of sleepThreadWait()
+	
+	
 	public static void myAccount(WebDriver driver, WebDriverWait wait){
 		//Navigate to my account settings
 		
