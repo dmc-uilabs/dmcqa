6fac59b21860402668e478643a060a5cb05ee6fe
arsalan17chaudhry <arsalan17@live.com>
2017-08-23 15:13:58 -0500

    Update LoginPage.java

--- dmcQA/src/main/java/pageObjects/LoginPage.java
+++ dmcQA/src/main/java/pageObjects/LoginPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 public class LoginPage {
 public WebDriver driver;
 
-	String Selenium_User1 = "dmctest.uilabs@gmail.com";
-	String Selenium_Pass1 = "ilvqQ.;%Q*Ce=U6X4?4$";
 
 	By emailUILabs = By.id("cred_userid_inputtext");
 	By passwordUILabs=By.id("cred_password_inputtext");
@@ -24,9 +22,7 @@ public WebDriver driver;
 	By pwGoogle = By.cssSelector("input[type='password']");
 	By submitGoogle = By.id("passwordNext");
 	
-	//Site: https://uilabs.idp.cirrusidentity.com/module.php/core/loginuserpass.php?AuthState=_a6f3506db4fd21bfd29316061cc77ee74d8e74cfb0%3Ahttps%3A%2F%2Fuilabs.idp.cirrusidentity.com%2Fsaml2%2Fidp%2FSSOService.php%3Fspentityid%3Dhttps%253A%252F%252Fdev-web1.opendmc.org%252Fshibboleth%26cookieTime%3D1501467976%26RelayState%3Dss%253Amem%253Ad956c692306bdb16483d8b3ace7e3adaaf1de97357f1a85784da77afc159b476
-	//Site: https://accounts.google.com/signin/oauth/identifier?client_id=220522369979-opd8pck88l8mms84jsr03v4o079ohi7s.apps.googleusercontent.com&as=54a6df65463e617e&destination=https%3A%2F%2Fprojectdmc-dev-web1.cirrusidentity.com&approval_state=!ChRKRTVIMms3Qzd5c2ZMdVlJVzBlcRIfZzlxWXNMdmJhM0FVa0FoVURVeHIzUl9LMG5objJSVQ%E2%88%99ADiIGyEAAAAAWX_m4_fTxYfIAK7JV02dYxayRS18jJT4&passive=1209600&oauth=1&sarp=1&scc=1&xsrfsig=AHgIfE9u4zd5a65I1HVNMYc3Z6P8vpcHBA&flowName=GeneralOAuthFlow
-
+	
 	public LoginPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 		this.driver=driver;
