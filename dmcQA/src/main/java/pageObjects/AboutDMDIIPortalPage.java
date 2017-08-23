8f0e95ee9f170039fde0d2a20d7bae67c67a76df
arsalan17chaudhry <arsalan17@live.com>
2017-08-23 12:46:01 -0500

    page objects

---
+++ dmcQA/src/main/java/pageObjects/AboutDMDIIPortalPage.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class AboutDMDIIPortalPage {
+	public WebDriver driver;
+	
+	public AboutDMDIIPortalPage(WebDriver driver) {
+		//Auto-generated constructor stub
+		this.driver=driver;
+	}
+}
